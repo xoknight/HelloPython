afcf749401ea76fe59795c9ad331e155cecde31b
xoknight <xoknight@gmail.com>
2015-01-14 22:03:37 +0800

    第一次提交

---
+++ Test/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#coding=utf-8
+print "欢迎老梁来到python的世界！"
+item_one = 1
+item_two = 2
+item_three =3
+#注\后面不能加东西
+total = item_one + \
+        item_two + \
+        item_three
+print total
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
+print days[1] #python数组引用
